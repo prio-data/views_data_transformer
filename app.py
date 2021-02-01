571da1c2e3cafd596a977ce154ffea230aa272f6
peder2911 <pglandsverk@gmail.com>
2021-02-01 15:05:27 +0100

    Timelag
    Added a timelag-transform.
    
    This transform requires data from before the queried data, so I had to
    pass the RHS path to the transform function so it can query for it. This
    should be fine, I thought it would make writing tests a little harder,
    but it wasn't a problem.
    
    The tlag works great! Now we can start writing more transforms.

--- app.py
+++ app.py
@@ -11,23 +11,34 @@ import fastapi
 
 import pandas as pd
 import url_args
+from transforms import timelag
 
 ROUTER_URL = "http://0.0.0.0:8000"
 
 app = fastapi.FastAPI()
 
+"""
+Each transform takes two arguments:
+
+    rhs, which is the URL to the router resource to be transformed
+    The dataset df, which is retrieved from the rhs
+"""
+
 TRANSFORMS = {
         "priogrid_month":{
-            "identity":lambda x: x
+            "identity":lambda r,df: df,
+            "tlag":timelag
             },
         "country_month":{
-            "identity":lambda x: x
+            "identity":lambda r,df: df,
+            "tlag":timelag
             }
         }
 
-@app.get("/{loa}/{transform_name}/{params}/{rhs:path}")
-def transform(loa:str,transform_name:str,url_args_raw:url_args.url_args,rhs:str):
-    rhs_request = requests.get(os.path.join(ROUTER_URL,loa,rhs))
+@app.get("/{loa}/{transform_name}/{url_args_raw}/{rhs:path}")
+def transform(loa:str, transform_name:str, url_args_raw:url_args.url_args, rhs:str):
+    rhs_url = os.path.join(ROUTER_URL,loa,rhs)
+    rhs_request = requests.get(rhs_url)
 
     if not rhs_request.status_code == 200:
         return fastapi.Response(f"Router returned {rhs_request.status_code} "
@@ -47,15 +58,16 @@ def transform(loa:str,transform_name:str,url_args_raw:url_args.url_args,rhs:str)
     except KeyError:
         return fastapi.Response(f"Transform not found: {loa}>{transform_name}", status_code=404)
 
-    args = [data]
+    args = [rhs_url,data]
     if url_args_raw == "_":
         pass
     else:
-        args = url_args.parse(url_args_raw) + args
+        args = args + url_args.parse(url_args_raw)
         
     try:
         data = fn(*args)
     except TypeError as e:
+        # Wrong number of arguments
         return fastapi.Response(content=str(e),status_code=400)
 
     fake_file = io.BytesIO()
---
+++ transforms.py
@@ -0,0 +1,48 @@
+import os
+import io
+import math
+import fastapi
+import requests
+import pandas as pd
+import numpy as np
+
+def lag_by_index(df,lag_size,index_no=0):
+    """
+    "lags" the specified index 
+    """
+    idx_names = df.index.names
+    if idx_names[0] is None:
+        idx_names = ["index"]
+
+    df = df.reset_index()
+    df[idx_names[index_no]]+=lag_size
+    df = df.set_index(idx_names)
+    return df
+
+
+def timelag(rhs_url: str, df:pd.DataFrame, lag_size:int):
+    """
+    This function exposes lag_by_index, handling the retrieval of data for 
+    the previous year(s) to carry over.
+    """
+
+    tindices = np.array([i[0] for i in df.index.values])
+    start,end = tindices.min(),tindices.max()
+    year_unit_size = (end-start)+1
+    tlag_year_size = (lag_size / year_unit_size) 
+
+    for i in range(math.ceil(tlag_year_size)):
+        prev_url,yr = os.path.split(rhs_url)
+        prev_url = os.path.join(prev_url,str(int(yr)-(i+1)))
+
+        prev_request = requests.get(prev_url)
+
+        if prev_request.status_code != 200:
+            raise requests.HTTPError
+
+        prev_data = pd.read_parquet(io.BytesIO(prev_request.content))
+        df = pd.concat([prev_data,df])
+
+    df = lag_by_index(df,lag_size)
+    df.columns = [c+"_tlag_"+str(lag_size) for c in df.columns]
+    return df.loc[start:end,:]
