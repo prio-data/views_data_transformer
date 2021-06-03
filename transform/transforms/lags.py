d86ba8e1c7d7b8af365c77b780be3cfcf769485a
peder2911 <pglandsverk@gmail.com>
2021-06-03 12:48:40 +0200

    Added Jim's spatial lag
    Also bumped the required VTL version

--- transform/transforms/lags.py
+++ transform/transforms/lags.py
@@ -1,6 +1,8 @@
 
-from views_transformation_library import views_2
+from views_transformation_library import views_2,splag4d
 from . import porting
 
 tlag = porting.vectorize_across_dataframe(views_2.tlag)
 tlead = porting.vectorize_across_dataframe(views_2.tlead)
+
+get_splag4d = splag4d.get_splag4d
