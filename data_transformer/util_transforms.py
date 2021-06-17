c67cb0d21df11a5ed23650d46c365802dbb3ce76
peder2911 <pglandsverk@gmail.com>
2021-06-17 16:35:22 +0200

    Hotfix of rename function breaking
    The rename function is still relying on a hack to work with _ in names. I re-added the hack, but I should look into writing a better solution

--- data_transformer/util_transforms.py
+++ data_transformer/util_transforms.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-def rename(df: pd.DataFrame, new_name: str):
+def rename(df: pd.DataFrame, *args):
     """
     Rename the first column in a data-frame to the provided string
     """
+    new_name = "_".join([str(a) for a in args])
     return df.rename(columns = {df.columns[0]: new_name})
