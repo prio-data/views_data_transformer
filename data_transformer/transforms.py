81ea498eb45a20d6ba8fb7be70515dee1e51e0e5
jimdale <jimdale@users.noreply.github.com>
2021-11-04 11:33:44 +0100

    Add splag_country to registry, improved docstrings for all lags

--- data_transformer/transforms.py
+++ data_transformer/transforms.py
@@ -119,6 +119,11 @@ registry.register_function(
         lags.spatial_lag,
         "spatial","lag", applicable_to = ["priogrid_month"],
         )
+        
+ registry.register_function(
+        lags.spatial_lag_country,
+        "spatial","countrylag", applicable_to = ["country_month"],
+        )       
 
 registry.register_function(
         lags.spatial_tree_lag,
